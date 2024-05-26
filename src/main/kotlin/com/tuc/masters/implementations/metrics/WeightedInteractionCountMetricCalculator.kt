562ea7fb117aad508c9c8269d3ad94a909f7c6ff
paekva <ekaterina.pavlova@siemens-energy.com>
2024-05-26 19:24:53 +0200

    Fix metric

--- src/main/kotlin/com/tuc/masters/implementations/metrics/WeightedInteractionCountMetricCalculator.kt
+++ src/main/kotlin/com/tuc/masters/implementations/metrics/WeightedInteractionCountMetricCalculator.kt
@@ -16,13 +16,37 @@ class WeightedInteractionCountMetricCalculator: MetricCalculator {
         )
         set(_) {}
 
+
+    // 1.10 s for pointing
+    // 0.20s for btn click or keystroke
+    // 0.40s homing (changing input device)
     override fun calculateSingleTestMetric(parsedData: ParsedData): Double {
-        return parsedData.actions.sumOf {interactionToWeightMap[it.type] ?: 0}.toDouble()
+        /// I think here we need to fo not just sum but take into account sequence, so that we can include homing
+        /// click: pointing + click
+        /// send key: click * input length
+        /// and maybe rename to like KLM time or smth
+
+        var sum = 0.0
+        var isKeyboard = parsedData.actions.firstOrNull()?.type == ActionType.SEND_KEYS
+        for (a in parsedData.actions) {
+            if(a.type == ActionType.SEND_KEYS) {
+                val number = getNumberOfKeys(a.args ?: "") ?: 1
+                sum += 0.2 * number
+            }
+            if(a.type == ActionType.CLICK) sum += 1.3
+
+            if (isKeyboard && a.type != ActionType.SEND_KEYS || !isKeyboard && a.type == ActionType.SEND_KEYS) {
+                isKeyboard = !isKeyboard
+                sum += 0.4
+            }
+        }
+        return sum
     }
 
-    private val interactionToWeightMap: Map<ActionType, Int> = mapOf(
-        ActionType.CLICK to 1,
-        ActionType.SEND_KEYS to 2,
-        ActionType.SCROLL to 3,
-    )
+    private fun getNumberOfKeys(args: String): Int? {
+        val rgx = Regex("\"text\"\\s*:\\s*\"(?<name>[\\S\\s]*)\"")
+        val res = rgx.find(args) ?: return null
+
+        return res.groups[1]?.value?.length
+    }
 }
